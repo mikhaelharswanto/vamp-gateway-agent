b14fa172dc427bd14c848ea8f25246692cd51df4
Mikhael Tanutama <mikhael.tanutama@nokia.com>
2017-01-12 11:50:29 -0800

    Add defensive previously blocked ports cleanup at reload

--- reload.sh
+++ reload.sh
@@ -13,6 +13,13 @@ function haproxy_reload() {
     haproxy -f ${configuration} -p ${pid_file} -D -st $(cat ${pid_file})
 }
 
+function cleanup_previously_blocked() {
+    iptables -L -v | grep DROP | grep flags:FIN,SYN,RST,ACK/SYN | while read -r entry ; do
+      port=`echo ${entry} | awk {'print $11'} | awk -F: {'print $2'}`
+      iptables -w -D INPUT -p tcp --dport ${port} --syn -j DROP
+    done
+}
+
 function block_traffic() {
     # for zero downtime HAProxy reload: http://engineeringblog.yelp.com/2015/04/true-zero-downtime-haproxy-reloads.html
     # and for this implementation also: https://github.com/mesosphere/marathon-lb/blob/master/service/haproxy/run
@@ -40,6 +47,7 @@ do
     fi
 done < "${configuration}"
 
+cleanup_previously_blocked
 block_traffic
 sleep 0.1
 haproxy_reload
