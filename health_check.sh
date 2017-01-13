1400839c3d0934293f5aa4121026bdc994894506
Mikhael Tanutama <mikhael.tanutama@nokia.com>
2017-01-12 16:03:22 -0800

    Modify health check to use the HaProxy statistics endpoint for verification

--- health_check.sh
+++ health_check.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-function validate_sockets() {
-    observed_nof_sockets=`ls /usr/local/vamp/*.sock | grep -iv haproxy.log.sock | wc -l`
-    expected_nof_sockets=`cat /usr/local/vamp/haproxy.cfg | grep bind | grep .sock | uniq | wc -l`
-    if [ "x$observed_nof_sockets" == "x$expected_nof_sockets" ]; then
+function validate_backends() {
+    down_services=`curl -s "http://127.0.0.1:1988/;csv;norefresh" | grep DOWN | wc -l`
+    if [ "x$down_services" == "x0" ]; then
         echo 'true'
     else
         echo 'false'
     fi
 }
 
-
-if [ "x`validate_sockets`" == "xfalse" ]; then
+if [ "x`validate_backends`" == "xfalse" ]; then
     exit -1
 else
     exit 0
